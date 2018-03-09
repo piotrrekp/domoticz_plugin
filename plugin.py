2303a597e0fc76e7bd367c6d7d83ce386f99f50f
piotrrek <piotrrek1988@gmail.com>
2018-03-09 11:12:23 +0100

    10seconds timeout added

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
     <params>
        <param field="Mode1" label="Timeout" width="75px" required="true">
             <options>
-                <option label="1 minute" value="0.1"/>
+                <option label="10 seconds" value="0.1"/>
                 <option label="30 seconds" value="0.5" default="true" />
                 <option label="1 minute" value="1.0"/>
                 <option label="2 minutes" value="2.0"/>
